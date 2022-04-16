4c51891ef26bda6be5a201f1e7d340be615c57ee
Ofeki <ofekalm100@gmail.com>
2022-04-17 00:42:38 +0300

    little bit of changes
    Co-authored-by: shaharmazor8@gmail.com <shaharmazor8@gmail.com>

--- MainApp.js
+++ MainApp.js
@@ -1,6 +1,6 @@
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
-const { Client, LegacySessionAuth, Buttons, MessageTypes } = require('whatsapp-web.js');
+const { Client, LegacySessionAuth, Buttons, MessageTypes, GroupNotificationTypes } = require('whatsapp-web.js');
 
 // Path where the session data will be stored
 const SESSION_FILE_PATH = './session.json';
@@ -19,8 +19,8 @@ const client = new Client({
     })
 });
 
-// Save session values to the file upon successful auth
-client.on('authenticated', (session) => {
+// Save session values to the file upon successful auth                            
+client.on('authenticated', (session) => { 
     sessionData = session;
     fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
         if (err) {
@@ -58,60 +58,95 @@ client.on('message', async message => {
     if(message.body === '!הקלט'){
         await chat.sendStateRecording();
     }
-    
-    if(message.body === '!כפתור'){
-        let button = new Buttons('זה עובד',[{body:'אופקי',id:"ofek"},{body:'המלך',id:"theking"}],'זה בדיקה לכפתור בווצאפ','כל הזכויות שמורות');
-        
-        //console.log("This is the id of the btn shown up - "+id_Btn);
+    // 
+    if(message.body.startsWith('!כפתור')){
+        let button;
+        let jsonfile = JSON.parse(fs.readFileSync("./buttons.json"));
+        let content = message.body.slice(7);
+        let content_arr = content.split("-");
+        let title = content_arr[0];
+        let discription = content_arr[1];
+        let arr_of_buttons_body = [];
+        for(let i = 2; i < arr_of_buttons_body.length; i++){
+            arr_of_buttons_body.append(content_arr[i]);
+        } 
+        console.log(jsonfile);
+        console.log(JSON.stringify(jsonfile));
+        let container = {
+            "title": title,
+            "discription": discription,
+            "arr_of_buttons_body": arr_of_buttons_body
+        };
+        //jsonfile.append(container);
+        fs.writeFile("./buttons.json", JSON.stringify(jsonfile, null, 2), err => {
+            if(err){
+                console.log(err);
+            }
+        });
+        if(arr_of_buttons_body.length === 2){
+            button = new Buttons(title,[{body:arr_of_buttons_body[0], id:arr_of_buttons_body[0]},{body:arr_of_buttons_body[1],id:arr_of_buttons_body[1]}], discription);
+        }
+        else if(arr_of_buttons_body.length === 3){
+            button = new Buttons(title,[{body:arr_of_buttons_body[0], id:arr_of_buttons_body[0]},{body:arr_of_buttons_body[1],id:arr_of_buttons_body[1]}, {body:arr_of_buttons_body[2], id:arr_of_buttons_body[2]}], discription);
+        }
+        else if(arr_of_buttons_body.length === 4){
+            button = new Buttons(title,[{body:arr_of_buttons_body[0], id:arr_of_buttons_body[0]},{body:arr_of_buttons_body[1],id:arr_of_buttons_body[1]}, {body:arr_of_buttons_body[2], id:arr_of_buttons_body[2]}, {body:arr_of_buttons_body[3], id:arr_of_buttons_body[3]}], discription);
+        }
+        else if(arr_of_buttons_body.length === 5){
+            button = new Buttons(title,[{body:arr_of_buttons_body[0], id:arr_of_buttons_body[0]},{body:arr_of_buttons_body[1],id:arr_of_buttons_body[1]}, {body:arr_of_buttons_body[2], id:arr_of_buttons_body[2]}, {body:arr_of_buttons_body[3], id:arr_of_buttons_body[3]}, {body:arr_of_buttons_body[4], id:arr_of_buttons_body[4]}], discription);
+        }
         await chat.sendMessage(button);
-        //console.log("the btn id "+button.buttons);
-        
-
-        await message.reply("aolala");
-        let f = message.hasQuotedMsg;
-        //console.log(f);
     }
     
     if(message.type === MessageTypes.BUTTONS_RESPONSE){
-        
-        let rawdata = fs.readFileSync(COUNTER_FILE_PATH);
-        let content = JSON.parse(rawdata);
-        let count = content.CounterOne;
-        console.log("            "+content);
-        console.log("            "+count);
-
+        let jsonfile = JSON.parse(fs.readFileSync("./counter.json"));   
         if(message.selectedButtonId === "theking"){
-            
+            jsonfile.CounterOne++;
         }
         else{
-            twocounter++;
+            jsonfile.CounterTwo++;
         }
+
+        fs.writeFile("./counter.json", JSON.stringify(jsonfile, null, 2), err => {
+            if(err){
+                console.log(err);
+            }
+        });
     }
     if(message.body === "!stop"){
-        chat.sendMessage("The number of counter is - "+counter);
-        chat.sendMessage("The number of twocounter is - "+twocounter);
+        let jsonfile = JSON.parse(fs.readFileSync("./counter.json"));
+        chat.sendMessage("The number of counter is - "+ counter);
+        chat.sendMessage("The number of twocounter is - "+ twocounter);
     }
     if(message.body.startsWith("!קלט")){
         let ret = message.body.slice(5);
         await chat.sendMessage('אמרת: ' + ret);
     }
-    if(message.body.startsWith("!sendlove")){
-        let ret = message.body.slice(10);
-        for (let i = 0; i <= ret.length; i++) {
-            await chat.sendMessage("Shahar ily ma babe");
+    if(message.body.startsWith("!spam")){
+        let ret = message.body.split(" ");
+        let ret_num = (Number)(ret[1]);
+        console.log("The ret is - "+ret_num);
+        let spam_array = message.body.split(" ");
+        console.log(spam_array);
+        let spam_msg= "";
+        for(let i =2; i < spam_array.length;i++){
+            spam_msg += " "+spam_array[i];
+        }
+        for (let i = 0; i < ret_num; i++) {
+            await chat.sendMessage(spam_msg);
+            console.log(spam_msg+"           "+spam_array+"      f");
         }
     }
-    
+    if(message.body.startsWith("!צרף")){
+        //reaction command
+        let quoted = message.getQuotedMessage();
+    }
+    console.log(message.body);
     if(message.type === MessageTypes.BUTTONS_RESPONSE){
         console.log();
         
     }
-    console.log(message.type);
-});
-client.on('message_create', async msg =>{
-    
-    
-    
+
 });
 
 // function timeDifference(current, previous){
